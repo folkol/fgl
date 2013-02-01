62034fb43a16f432327fba706a1029a678943369
Folkol <mattias4@kth.se>
2013-02-01 23:27:33 +0100

    Changed the rendering example to a basic colored clear

--- FGL/main.cpp
+++ FGL/main.cpp
@@ -8,20 +8,8 @@ public:
 	}
 
 	void render() {
-			static float theta = 0;
-			glClearColor( 0.0f, 0.0f, 0.0f, 0.0f );
-			glClear( GL_COLOR_BUFFER_BIT );
-			
-			glPushMatrix();
-			glRotatef( theta, 0.0f, 0.0f, 1.0f );
-			glBegin( GL_TRIANGLES );
-			glColor3f( 1.0f, 0.0f, 0.0f ); glVertex2f( 0.0f, 1.0f );
-			glColor3f( 0.0f, 1.0f, 0.0f ); glVertex2f( 0.87f, -0.5f );
-			glColor3f( 0.0f, 0.0f, 1.0f ); glVertex2f( -0.87f, -0.5f );
-			glEnd();
-			glPopMatrix();
-			
-			theta += 1.0f;
+		glClearColor(1.0f, 1.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
 	}
 };
 

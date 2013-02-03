ede5fd239c0f1f38dfe2aad2e5935af2cc32eb8c
Folkol <mattias4@kth.se>
2013-02-03 23:47:57 +0100

    Naive perspective projection

--- FGL/resources/square_2d/vertex.glsl
+++ FGL/resources/square_2d/vertex.glsl
@@ -10,25 +10,35 @@ void main()
 {
 	// Transforms that map worldspace to project space is 
 	// called projection matrix
-	mat3 window_scale = mat3(
+	mat3 otho_projection = mat3(
         vec3(3.0/4.0, 0.0, 0.0),
         vec3(    0.0, 1.0, 0.0),
         vec3(    0.0, 0.0, 1.0)
     );
+	mat3 otho_projection_with_scaled_down_z_axis = mat3(
+        vec3(3.0/4.0, 0.0, 0.0),
+        vec3(    0.0, 1.0, 0.0),
+        vec3(    0.0, 0.0, 0.5)
+    );
+
 	// Transforms to orient and place objects in world space
 	// Are called model-view matrices
-	mat3 rotation =
-		mat3(
-			vec3( cos(timer),  sin(timer),  0.0),
-			vec3(-sin(timer),  cos(timer),  0.0),
-			vec3(        0.0,         0.0,  1.0)
-		);
+    mat3 rotation = mat3(
+        vec3(1.0,         0.0,         0.0),
+        vec3(0.0,  cos(timer),  sin(timer)),
+        vec3(0.0, -sin(timer),  cos(timer))
+    );
 	mat3 object_scale = mat3(
         vec3(4.0/3.0, 0.0, 0.0),
         vec3(    0.0, 1.0, 0.0),
         vec3(    0.0, 0.0, 1.0)
     );
-    gl_Position = vec4(window_scale * rotation * object_scale * position.xyz, 1.0);
+
+	vec3 projected_position = otho_projection_with_scaled_down_z_axis * rotation * object_scale * position.xyz;
+	float perspective_factor = projected_position.z * 0.5 + 1.0;
+
+    gl_Position = vec4(projected_position/perspective_factor, 1.0);
+
     texcoord = position.xy * vec2(0.5) + vec2(0.5);
 	fade_factor = sin(timer) * 0.5 + 0.5;
 }
